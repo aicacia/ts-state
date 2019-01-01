37ffc673357daa1ad7add67e10f3359602252add
Nathan Faucett <nathanfaucett@gmail.com>
2019-01-01 10:45:54 -0500

    clean up

--- lib/index.ts
+++ lib/index.ts
@@ -1,170 +1,168 @@
 import { EventEmitter } from "events";
 
 export type IStores<S> = {
-    [K in Extract<keyof S, string>]: Store<State<S>, S[K]>
+  [K in Extract<keyof S, string>]: Store<State<S>, S[K]>
 };
 
 export class Store<S, T> extends EventEmitter {
-    name: Extract<keyof S, string>;
-    state: State<S>;
+  name: Extract<keyof S, string>;
+  state: State<S>;
 
-    constructor(state: State<S>, name: Extract<keyof S, string>) {
-        super();
+  constructor(state: State<S>, name: Extract<keyof S, string>) {
+    super();
 
-        this.name = name;
-        this.state = state;
-    }
+    this.name = name;
+    this.state = state;
+  }
 
-    getName(): Extract<keyof S, string> {
-        return this.name;
-    }
+  getName(): Extract<keyof S, string> {
+    return this.name;
+  }
 
-    getState(): T {
-        return this.state.getStateFor(this.name) as any;
-    }
+  getState(): T {
+    return this.state.getStateFor(this.name) as any;
+  }
 
-    setState(state: T, meta?: any): Store<S, T> {
-        this.emit("set-state", state);
-        this.state.setStateFor(this.name, state as any, meta);
-        return this;
-    }
+  setState(state: T, meta?: any): Store<S, T> {
+    this.emit("set-state", state);
+    this.state.setStateFor(this.name, state as any, meta);
+    return this;
+  }
 
-    updateState(fn: (prev: T) => T, meta?: any): Store<S, T> {
-        return this.setState(fn(this.getState()), meta);
-    }
+  updateState(fn: (prev: T) => T, meta?: any): Store<S, T> {
+    return this.setState(fn(this.getState()), meta || fn.name);
+  }
 
-    noEmitSetState(state: T): Store<S, T> {
-        this.state.noEmitSetStateFor(this.name, state as any);
-        return this;
-    }
+  noEmitSetState(state: T): Store<S, T> {
+    this.state.noEmitSetStateFor(this.name, state as any);
+    return this;
+  }
 
-    noEmitUpdateState(fn: (prev: T) => T): Store<S, T> {
-        return this.noEmitSetState(fn(this.getState()));
-    }
+  noEmitUpdateState(fn: (prev: T) => T): Store<S, T> {
+    return this.noEmitSetState(fn(this.getState()));
+  }
 
-    toJSON(): any {
-        return this.getState();
-    }
+  toJSON(): any {
+    return this.getState();
+  }
 
-    fromJSON(json: any): T {
-        return json;
-    }
+  fromJSON(json: any): T {
+    return json;
+  }
 
-    setStateJSON(json: any): Store<S, T> {
-        return this.setState(this.fromJSON(json));
-    }
+  setStateJSON(json: any): Store<S, T> {
+    return this.setState(this.fromJSON(json));
+  }
 
-    noEmitSetStateJSON(json: any): Store<S, T> {
-        return this.noEmitSetState(this.fromJSON(json));
-    }
+  noEmitSetStateJSON(json: any): Store<S, T> {
+    return this.noEmitSetState(this.fromJSON(json));
+  }
 }
 
 export class State<S> extends EventEmitter {
-    current: S;
-    stores: IStores<S>;
+  current: S;
+  stores: IStores<S>;
 
-    constructor(state: S) {
-        super();
+  constructor(state: S) {
+    super();
 
-        const stores = {} as IStores<S>;
+    const stores = {} as IStores<S>;
 
-        for (let key in state) {
-            (stores as any)[key] = new Store(this, key);
-        }
-
-        this.current = state;
-        this.stores = stores;
+    for (let key in state) {
+      (stores as any)[key] = new Store(this, key);
     }
 
-    getStore<K extends Extract<keyof S, string>>(
-        name: K
-    ): Store<State<S>, S[K]> {
-        return this.stores[name];
+    this.current = state;
+    this.stores = stores;
+  }
+
+  getStore<K extends Extract<keyof S, string>>(name: K): Store<State<S>, S[K]> {
+    return this.stores[name];
+  }
+
+  getStateFor<K extends keyof S>(name: K): S[K] {
+    return this.current[name];
+  }
+
+  getState(): S {
+    return this.current;
+  }
+
+  setStateFor<K extends Extract<keyof S, string>>(
+    name: K,
+    state: S[K],
+    meta?: any
+  ): State<S> {
+    return this.internalSetStateFor(name, state, meta, true);
+  }
+
+  setState(state: S, meta?: any): State<S> {
+    return this.internalSetState(state, meta, true);
+  }
+
+  noEmitSetStateFor<K extends Extract<keyof S, string>>(
+    name: K,
+    state: S[K],
+    meta?: any
+  ): State<S> {
+    return this.internalSetStateFor(name, state, meta, false);
+  }
+
+  noEmitSetState(state: S): State<S> {
+    return this.internalSetState(state, false);
+  }
+
+  toJSON(): S {
+    return this.getState();
+  }
+
+  fromJSON(json: any): S {
+    return Object.keys(json).reduce((state, name) => {
+      const store = this.getStore(name as any),
+        storeJSON = json[name];
+
+      if (store && storeJSON) {
+        (state as any)[name] = store.fromJSON(storeJSON);
+      }
+
+      return state;
+    }, this.current);
+  }
+
+  setStateJSON(json: any): State<S> {
+    return this.setState(this.fromJSON(json));
+  }
+
+  noEmitSetStateJSON(json: any): State<S> {
+    return this.noEmitSetState(this.fromJSON(json));
+  }
+
+  internalSetStateFor<K extends Extract<keyof S, string>>(
+    name: K,
+    state: S[K],
+    meta?: any,
+    emit: boolean = true
+  ): State<S> {
+    const nextState: S = { ...(this.current as any) };
+
+    nextState[name] = state as any;
+
+    this.internalSetState(nextState, meta, emit);
+
+    if (emit) {
+      this.emit("set-state-for", name, state, meta);
     }
 
-    getStateFor<K extends keyof S>(name: K): S[K] {
-        return this.current[name];
+    return this;
+  }
+
+  internalSetState(state: S, meta?: any, emit: boolean = true): State<S> {
+    this.current = state;
+
+    if (emit) {
+      this.emit("set-state", state, meta);
     }
 
-    getState(): S {
-        return this.current;
-    }
-
-    setStateFor<K extends Extract<keyof S, string>>(
-        name: K,
-        state: S[K],
-        meta?: any
-    ): State<S> {
-        return this.internalSetStateFor(name, state, meta, true);
-    }
-
-    setState(state: S, meta?: any): State<S> {
-        return this.internalSetState(state, meta, true);
-    }
-
-    noEmitSetStateFor<K extends Extract<keyof S, string>>(
-        name: K,
-        state: S[K],
-        meta?: any
-    ): State<S> {
-        return this.internalSetStateFor(name, state, meta, false);
-    }
-
-    noEmitSetState(state: S): State<S> {
-        return this.internalSetState(state, false);
-    }
-
-    toJSON(): S {
-        return this.getState();
-    }
-
-    fromJSON(json: any): S {
-        return Object.keys(json).reduce((state, name) => {
-            const store = this.getStore(name as any),
-                storeJSON = json[name];
-
-            if (store && storeJSON) {
-                (state as any)[name] = store.fromJSON(storeJSON);
-            }
-
-            return state;
-        }, this.current);
-    }
-
-    setStateJSON(json: any): State<S> {
-        return this.setState(this.fromJSON(json));
-    }
-
-    noEmitSetStateJSON(json: any): State<S> {
-        return this.noEmitSetState(this.fromJSON(json));
-    }
-
-    internalSetStateFor<K extends Extract<keyof S, string>>(
-        name: K,
-        state: S[K],
-        meta?: any,
-        emit: boolean = true
-    ): State<S> {
-        const nextState: S = { ...(this.current as any) };
-
-        nextState[name] = state as any;
-
-        this.internalSetState(nextState, meta, emit);
-
-        if (emit) {
-            this.emit("set-state-for", name, state, meta);
-        }
-
-        return this;
-    }
-
-    internalSetState(state: S, meta?: any, emit: boolean = true): State<S> {
-        this.current = state;
-
-        if (emit) {
-            this.emit("set-state", state, meta);
-        }
-
-        return this;
-    }
+    return this;
+  }
 }
--- tests/State.test.ts
+++ tests/State.test.ts
@@ -2,93 +2,93 @@ import * as tape from "tape";
 import { State } from "../lib";
 
 tape("State createStore, setState, updateState", (assert: tape.Test) => {
+  const state = new State({ counter: { count: 0 } }),
+    counter = state.getStore("counter");
+
+  let eventSetStateCalled = false,
+    eventSetStateForCalled = false,
+    metas: string[] = [];
+
+  state.on("set-state", () => {
+    eventSetStateCalled = true;
+  });
+
+  state.on("set-state-for", (name, state, meta) => {
+    eventSetStateForCalled = true;
+    metas.push(meta);
+    assert.equals(name, "counter");
+  });
+
+  counter.setState({ count: 1 }, "reset");
+  counter.updateState(({ count }) => ({ count: count + 1 }), "incement");
+
+  assert.deepEquals(metas, ["reset", "incement"]);
+
+  assert.deepEquals(state.getStateFor("counter"), { count: 2 });
+  assert.deepEquals(state.getState(), { counter: { count: 2 } });
+  assert.deepEquals(counter.getState(), { count: 2 });
+
+  assert.equals(eventSetStateCalled, true);
+  assert.equals(eventSetStateForCalled, true);
+
+  assert.end();
+});
+
+tape(
+  "State no emit createStore, setState, updateState",
+  (assert: tape.Test) => {
     const state = new State({ counter: { count: 0 } }),
-        counter = state.getStore("counter");
+      counter = state.getStore("counter");
 
     let eventSetStateCalled = false,
-        eventSetStateForCalled = false,
-        metas: string[] = [];
+      eventSetStateForCalled = false;
+
+    const reset = () => {
+      eventSetStateCalled = false;
+      eventSetStateForCalled = false;
+    };
 
     state.on("set-state", () => {
-        eventSetStateCalled = true;
+      eventSetStateCalled = true;
     });
 
-    state.on("set-state-for", (name, state, meta) => {
-        eventSetStateForCalled = true;
-        metas.push(meta);
-        assert.equals(name, "counter");
+    state.on("set-state-for", name => {
+      eventSetStateForCalled = true;
+      assert.equals(name, "counter");
     });
 
     counter.setState({ count: 1 }, "reset");
-    counter.updateState(({ count }) => ({ count: count + 1 }), "incement");
-
-    assert.deepEquals(metas, ["reset", "incement"]);
-
-    assert.deepEquals(state.getStateFor("counter"), { count: 2 });
-    assert.deepEquals(state.getState(), { counter: { count: 2 } });
-    assert.deepEquals(counter.getState(), { count: 2 });
+    assert.deepEquals(counter.getState(), { count: 1 });
 
     assert.equals(eventSetStateCalled, true);
     assert.equals(eventSetStateForCalled, true);
 
+    reset();
+
+    counter.noEmitSetState({ count: 0 });
+    assert.deepEquals(counter.getState(), { count: 0 });
+
+    assert.equals(eventSetStateCalled, false);
+    assert.equals(eventSetStateForCalled, false);
+
     assert.end();
-});
-
-tape(
-    "State no emit createStore, setState, updateState",
-    (assert: tape.Test) => {
-        const state = new State({ counter: { count: 0 } }),
-            counter = state.getStore("counter");
-
-        let eventSetStateCalled = false,
-            eventSetStateForCalled = false;
-
-        const reset = () => {
-            eventSetStateCalled = false;
-            eventSetStateForCalled = false;
-        };
-
-        state.on("set-state", () => {
-            eventSetStateCalled = true;
-        });
-
-        state.on("set-state-for", name => {
-            eventSetStateForCalled = true;
-            assert.equals(name, "counter");
-        });
-
-        counter.setState({ count: 1 }, "reset");
-        assert.deepEquals(counter.getState(), { count: 1 });
-
-        assert.equals(eventSetStateCalled, true);
-        assert.equals(eventSetStateForCalled, true);
-
-        reset();
-
-        counter.noEmitSetState({ count: 0 });
-        assert.deepEquals(counter.getState(), { count: 0 });
-
-        assert.equals(eventSetStateCalled, false);
-        assert.equals(eventSetStateForCalled, false);
-
-        assert.end();
-    }
+  }
 );
 
 tape("State/Store toJSON fromJSON", (assert: tape.Test) => {
-    const state = new State({ counter: { count: 0 } }),
-        counter = state.getStore("counter");
+  const state = new State({ counter: { count: 0 } }),
+    counter = state.getStore("counter");
 
-    assert.deepEquals(counter.toJSON(), { count: 0 });
-    counter.setStateJSON({ count: 2 });
-    assert.deepEquals(counter.toJSON(), { count: 2 });
+  assert.deepEquals(counter.toJSON(), { count: 0 });
+  counter.setStateJSON({ count: 2 });
+  assert.deepEquals(counter.toJSON(), { count: 2 });
 
-    state.setStateJSON({
-        counter: { count: 1 }
-    });
-    assert.deepEquals(state.toJSON(), {
-        counter: { count: 1 }
-    });
+  state.setStateJSON({
+    counter: { count: 1 }
+  });
+  assert.deepEquals(state.toJSON(), {
+    counter: { count: 1 }
+  });
 
-    assert.end();
+  assert.end();
 });
